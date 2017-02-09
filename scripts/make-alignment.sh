28b91595816256cd6f20267392200f6c71c12c21
Zhian Kamvar <zkamvar@unl.edu>
2017-02-09 14:38:03 -0600

    make-alignment looks like it works now

--- scripts/make-alignment.sh
+++ scripts/make-alignment.sh
@@ -21,19 +21,34 @@ fi
 IDX=$1
 DIR=$2
 # Slice the array from the 3rd position to the end
-# http://stackoverflow.com/a/1336245
+# http://stackoverflow.com/a/9057392
 SAMPLES="${@:3}"
 CMD="bowtie2 -x $IDX -S $DIR/"
 
 
 printf "" > runfiles/make-alignment.txt
 
+tmp=($SAMPLES)
+tmp=${tmp[0]}"_1.fq.gz"
+# Info Line: 
+#          0	     1	          2	      3	      4	5	   6	       7	         8
+# INSTRUMENT	RUN_NO	FLOWCELL_ID	LANE_NO	TILE_ID	X	Y	READ	FILTERED	CONTROL_NO
+INFO=($(zcat $tmp | head -n 1 | sed 's/:/ /g'))
+
 for s in $SAMPLES; do
+	# Create named pipes because bowtie2 is stupid and can't handle gzipped files >:(
 	MKFIFO="mkfifo \$TMPDIR/M1.fifo \$TMPDIR/M2.fifo"
 	M1=$s"_1.fq.gz"
 	M2=$s"_2.fq.gz"
 	WFIFO="zcat $M1 > \$TMPDIR/M1.fifo & zcat $M2 > \$TMPDIR/M2.fifo &"
+
+	# Setting arguments
 	ARGS="-1 \$TMPDIR/M1.fifo -2 \$TMPDIR/M2.fifo --maxins 800 --fr"
 	BASE=$(basename $s)
-	printf "$MKFIFO; $WFIFO $CMD$BASE $ARGS\n"
+	
+	# Setup read group information for GATK
+	RGID="${INFO[2]}.${INFO[3]}"
+	RG="--rg SM:$BASE PL:ILLUMINA LB:RUN.${INFO[1]}"
+
+	printf "$MKFIFO; $WFIFO $CMD$BASE $ARGS $RGID $RG\n" >> runfiles/make-alignment.txt
 done
